35e684b7187ebac2c961033660c4a3f633da7905
dhruvinparikh <imsilent93@gmail.com>
2021-01-26 20:14:08 -0500

    add truffle intro demo

--- notes/truffle/demo/metacoin/contracts/MetaCoin.sol
+++ notes/truffle/demo/metacoin/contracts/MetaCoin.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.4.25 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.8.0;
 
 import "./ConvertLib.sol";
 
@@ -12,7 +13,7 @@ contract MetaCoin {
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	constructor() public {
+	constructor() {
 		balances[tx.origin] = 10000;
 	}
 
--- notes/truffle/demo/metacoin/truffle-config.js
+++ notes/truffle/demo/metacoin/truffle-config.js
@@ -18,4 +18,22 @@ module.exports = {
   //  }
   //}
   //
+  compilers: {
+    solc: {
+      version: "0.7.1", // A version or constraint - Ex. "^0.5.0"
+                         // Can also be set to "native" to use a native solc
+      // { docker: <boolean>, // Use a version obtained through docker
+      // parser: "solcjs",  // Leverages solc-js purely for speedy parsing
+      // settings: {
+        // optimizer: {
+          // enabled: <boolean>,
+          // runs: <number>   // Optimize for how many times you intend to run the code
+        // },
+        // evmVersion: <string> // Default: "istanbul"
+      // }, }
+      // { modelCheckerSettings: {
+        // contains options for SMTChecker
+      // } }
+    }
+  }
 };
