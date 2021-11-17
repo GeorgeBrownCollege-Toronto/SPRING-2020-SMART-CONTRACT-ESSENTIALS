525a0713d7de0e52824f01e6ea00e64860aa847e
dhruvinparikh <imsilent93@gmail.com>
2021-11-16 21:28:08 -0500

    add env to metacoin

--- notes/sc-frameworks/demo/metacoin/truffle-config.js
+++ notes/sc-frameworks/demo/metacoin/truffle-config.js
@@ -1,39 +1,62 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
-  // Uncommenting the defaults below 
+  // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
   // You can also follow this format for other networks;
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
+  networks: {
+    //   development: {
+    //     host: "127.0.0.1",
+    //     port: 8545,
+    //     network_id: "*",
+    //   },
+    //  test: {
+    //    host: "127.0.0.1",
+    //    port: 7545,
+    //    network_id: "*"
+    //  }
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider({
+          privateKeys: [process.env.PRIVATE_KEY],
+          providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
+        });
+      },
+      network_id: "4",
+    },
+  },
   //
+  // Truffle DB is currently disabled by default; to enable it, change enabled:
+  // false to enabled: true. The default storage location can also be
+  // overridden by specifying the adapter settings, as shown in the commented code below.
+  //
+  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+  // make a backup of your artifacts to a safe location before enabling this feature.
+  //
+  // After you backed up your artifacts you can utilize db by running migrate as follows:
+  // $ truffle migrate --reset --compile-all
+  //
+  // db: {
+  // enabled: false,
+  // host: "127.0.0.1",
+  // adapter: {
+  //   name: "sqlite",
+  //   settings: {
+  //     directory: ".db"
+  //   }
+  // }
+  // }
   compilers: {
     solc: {
-      version: "0.7.1", // A version or constraint - Ex. "^0.5.0"
-                         // Can also be set to "native" to use a native solc
-      // { docker: <boolean>, // Use a version obtained through docker
-      // parser: "solcjs",  // Leverages solc-js purely for speedy parsing
-      // settings: {
-        // optimizer: {
-          // enabled: <boolean>,
-          // runs: <number>   // Optimize for how many times you intend to run the code
-        // },
-        // evmVersion: <string> // Default: "istanbul"
-      // }, }
-      // { modelCheckerSettings: {
-        // contains options for SMTChecker
-      // } }
-    }
-  }
+      version: "^0.8.10",
+    },
+  },
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
+  },
 };
